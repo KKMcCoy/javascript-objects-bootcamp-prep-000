d67eaed3aad20147d50d749d1ee24731f3177f1f
karen tsui <buzz.kyt@gmail.com>
2018-07-16 16:07:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { beatles: "yellow submarine" };
 
 function updatePlaylist(playlist, artist, title) {
-  return playlist.assign({},playlist, {artist:['title']});
+  return playlist.assign({}, playlist, {artist:['title']});
 }
 
